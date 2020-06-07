691b2261c950e49e906ab63cf43cbf093c6f4650
sara altaee <sara.korea96@gmail.com>
2020-06-07 20:36:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) =>{
+  let newArray = [array[0], array[1]]
+ return newArray
+}
+
+const returnLastTwoDrivers=(array)=>{
+  let newArray2 =[array[array.length-2],array[array.length-1]]
+  return newArray2;
+}
+let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+function createFareMultiplier(integer){
+  return 
+}
