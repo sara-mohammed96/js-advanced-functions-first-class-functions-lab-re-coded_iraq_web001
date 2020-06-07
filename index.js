6b601deb1b34d976c32f32a4cfc5d998915c3177
sara altaee <sara.korea96@gmail.com>
2020-06-07 20:55:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 function selectDifferentDrivers(array,functions){
-  return functions[array]
+  return functions(array);
 }
